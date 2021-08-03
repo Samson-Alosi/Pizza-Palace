a4b508f2d22593247ea856173823551d763cd8f5
Samson-Alosi <psalmchrisam@gmail.com>
2021-08-03 17:12:47 +0300

    adding JS script

---
+++ script.js
@@ -0,0 +1,23 @@
+/*jshint esversion: 6 */
+function proceed () {
+    var pizzaSize = document.getElementById('size').value;
+    var pizzaCrust = document.getElementById('crust').value;
+    var pizzaNeeded = document.getElementById('number').value;
+    var pizzaToppings = [];
+
+
+    var pizzaPrice;
+    if (pizzaCrust === "Cheese-Stuffed" || pizzaCrust === "Thin-Crust"){
+    if ()
+
+    }
+}
+
+
+
+
+
+
+
+
+
